dc3f955720c72c7878dcdcbf577e1feafb6ad854
A-San96 <assanediatta96@gmail.com>
2023-01-21 20:52:41 +0000

    add CRUD for Pizza and create http routes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.OpenApi.Models;
+using PizzaStore.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,11 @@ app.UseSwaggerUI(c => {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API v1");
 });
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/pizzas/{id}", (int id) => PizzaDB.GetPizza(id));
+app.MapGet("/pizzas", () => PizzaDB.GetPizzas());
+app.MapPost("/pizzas", (Pizza pizza) => PizzaDB.CreatePizza(pizza));
+app.MapPut("/pizzas", (Pizza pizza) => PizzaDB.UpdatePizza(pizza));
+app.MapDelete("/pizzas/{id}", (int id) => PizzaDB.RemovePizza(id));
+
 
 app.Run();
